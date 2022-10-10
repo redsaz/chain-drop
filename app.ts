6bdf3704de7de986eafc3f556cff9168aa2ce74b
redsaz <redsaz@gmail.com>
2022-10-10 00:09:28 -0500

    Allow cells to be placed on board
    There is a concept of active cells and "set" cells. Active cells can be
    moved around by player, and "set" cells are immovable. Movement of the
    active cells are affected by the "set" cells.

---
+++ app.ts
@@ -0,0 +1,232 @@
+/// <reference path="types/phaser.d.ts"t/>
+
+class SceneBackground extends Phaser.Scene {
+
+    constructor() {
+        super({ key: 'SceneBackground', active: true });
+    }
+
+    preload(): void {
+        this.load.image('background', 'assets/pics/background.jpg');
+    }
+
+    create(): void {
+        this.add.image(400, 300, 'background');
+    }
+
+    update(time: number, delta: number): void {
+    }
+}
+
+const cell_types_filter = 0b0000_1111;
+const cell_empty = 0b0000_0000;
+const cell_joined_top = 0b0001_0000;
+const cell_joined_right = 0b0010_0000;
+const cell_joined_bottom = 0b0100_0000;
+const cell_joined_left = 0b1000_0000;
+const cell_1 = 0b0000_0001;
+const cell_2 = 0b0000_0010;
+const cell_3 = 0b0000_0011;
+
+class SceneGrid extends Phaser.Scene {
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+    debugText: Phaser.GameObjects.Text | undefined;
+
+    grid_rows = 16;
+    grid_cols = 8;
+    grid: integer[] = Array(this.grid_rows * this.grid_cols);
+    cells_active: [number, number, integer][] = Array();
+
+    grid_display: (Phaser.GameObjects.Sprite | null)[] = Array(this.grid_rows * this.grid_cols);
+    cells_active_display: Phaser.GameObjects.Sprite[] = Array();
+
+    col_to_x(col: integer): integer {
+        // cols go from left (0) to right (7)
+        return col * 32 + 16;
+    }
+
+    row_to_y(row: integer): integer {
+        // rows go from bottom (0) to top (15), which is reverse of how pixels are done.
+        return 512 - (row * 32 + 16);
+    }
+
+    cell_to_scene(row: integer, col: integer, cell_value: integer): Phaser.GameObjects.Sprite | null {
+        let sprite: Phaser.GameObjects.Sprite;
+        // cols go from left (0) to right (7)
+        let x_pos = this.col_to_x(col);
+        // rows go from bottom (0) to top (15), which is reverse of how pixels are done.
+        let y_pos = this.row_to_y(row);
+        if (cell_value == 0) {
+            return null;
+        } else if ((cell_value & cell_joined_right) > 0) {
+            sprite = this.add.sprite(x_pos, y_pos, 'joined');
+        } else if ((cell_value & cell_joined_left) > 0) {
+            sprite = this.add.sprite(x_pos, y_pos, 'joined');
+            sprite.flipX = true;
+        } else if ((cell_value & cell_joined_top) > 0) {
+            sprite = this.add.sprite(x_pos, y_pos, 'joined');
+            sprite.setRotation(Math.PI / 2);
+            sprite.flipX = true;
+        } else if ((cell_value & cell_joined_bottom) > 0) {
+            sprite = this.add.sprite(x_pos, y_pos, 'joined');
+            sprite.setRotation(Math.PI / 2);
+        } else {
+            sprite = this.add.sprite(x_pos, y_pos, 'filled')
+        }
+        sprite.setScale(0.125, 0.125);
+
+        if ((cell_types_filter & cell_value) == 1) {
+            sprite.setTint(0xff0000);
+        } else if ((cell_types_filter & cell_value) == 2) {
+            sprite.setTint(0x00ff00);
+        } else if ((cell_types_filter & cell_value) == 3) {
+            sprite.setTint(0x4466ff);
+        }
+
+        return sprite;
+    }
+
+    grid_set(row: number, col: number, cell_value: integer) {
+        let index = row * this.grid_cols + col;
+        let old_cell = this.grid[index];
+        if (old_cell != cell_value) {
+            this.grid[index] = cell_value;
+            let sprite = this.grid_display[index];
+            if (sprite != null) {
+                sprite.destroy();
+            }
+            this.grid_display[index] = this.cell_to_scene(row, col, cell_value);
+        }
+    }
+
+    constructor() {
+        super({ key: 'SceneGrid', active: true });
+
+        for (let i = 0; i < this.grid_rows * this.grid_cols; ++i) {
+            this.grid[i] = cell_empty;
+            this.grid_display[i] = null;
+        }
+    }
+
+    cells_active_can_move(row_delta: number, col_delta: number): boolean {
+        if (this.cells_active.some((cell) => cell[1] + col_delta < 0)) {
+            return false;
+        } else if (this.cells_active.some((cell) => cell[1] + col_delta > this.grid_cols - 1)) {
+            return false;
+        } else if (this.cells_active.some((cell) => cell[0] + row_delta < 0)) {
+            return false
+        } else if (this.cells_active.some((cell) => cell[0] + row_delta > this.grid_rows - 1)) {
+            return false;
+        }
+        //return true;
+        return this.cells_active.every((cell) => this.grid[((cell[0] + row_delta) * this.grid_cols) + (cell[1] + col_delta)] == cell_empty);
+    }
+
+    preload(): void {
+        this.load.image('joined', 'assets/pics/joined.png');
+        this.load.image('filled', 'assets/pics/filled.png');
+        this.cameras.main.setViewport(272, 70, 256, 512)
+    }
+
+    create(): void {
+        this.add.rectangle(128, 256, 256, 512, 0, 0.5)
+        this.debugText = this.add.text(4, 4, 'NNN', { font: '20px Sans-Serif', color: '#000' });
+
+        // Add some obstacles on the board
+        this.grid_set(0, 0, cell_1);
+        this.grid_set(8, 3, cell_2);
+        this.grid_set(15, 7, cell_3);
+
+        // Add the active cells to board
+        this.cells_active.push([7, 3, cell_joined_right | cell_1], [7, 4, cell_joined_left | cell_2])
+        let left_active = this.add.sprite(this.col_to_x(3), this.row_to_y(7), 'joined');
+        left_active.setScale(0.125, 0.125);
+        left_active.setTint(0xff0000);
+        this.cells_active_display.push(left_active);
+        let right_active = this.add.sprite(this.col_to_x(4), this.row_to_y(7), 'joined');
+        right_active.setTint(0x00ff00);
+        right_active.setScale(0.125, 0.125);
+        right_active.flipX = true;
+        this.cells_active_display.push(right_active);
+        // this.grid_set(7, 3, cell_joined_right | cell_1)
+        // this.grid_set(7, 4, cell_joined_left | cell_2)
+
+        // this.cells_active.push([7, 3, cell_joined_bottom | cell_1], [6, 3, cell_joined_top | cell_3])
+        // this.grid_set(7, 3, cell_joined_bottom | cell_1)
+        // this.grid_set(6, 3, cell_joined_top | cell_3)
+        if (this.input.keyboard !== null) {
+            this.cursors = this.input.keyboard.createCursorKeys();
+        }
+    }
+
+    update(time: number, delta: number): void {
+        if (this.cursors !== undefined) {
+            if (this.cursors.left.isDown) {
+                // If the active cells can go left, then go.
+                if (this.cells_active_can_move(0, -1)) {
+                    this.cells_active.forEach((cell) => {
+                        // this.grid_set(cell[0], cell[1], cell_empty);
+                    });
+                    this.cells_active.forEach((cell) => {
+                        cell[1] -= 1;
+                        // this.grid_set(cell[0], cell[1], cell[2]);
+                    });
+                }
+            }
+            if (this.cursors.right.isDown) {
+                // If the active cells can go right, then go.
+                if (this.cells_active_can_move(0, 1)) {
+                    this.cells_active.forEach((cell) => {
+                        // this.grid_set(cell[0], cell[1], cell_empty);
+                    });
+                    this.cells_active.forEach((cell) => {
+                        cell[1] += 1;
+                        // this.grid_set(cell[0], cell[1], cell[2]);
+                    });
+                }
+            }
+            if (this.cursors.up.isDown) {
+                // If the active cells can go up, then go.
+                if (this.cells_active_can_move(1, 0)) {
+                    this.cells_active.forEach((cell) => {
+                        // this.grid_set(cell[0], cell[1], cell_empty);
+                    });
+                    this.cells_active.forEach((cell) => {
+                        cell[0] += 1;
+                        // this.grid_set(cell[0], cell[1], cell[2]);
+                    });
+                }
+            }
+            if (this.cursors.down.isDown) {
+                // If the active cells can go down, then go.
+                if (this.cells_active_can_move(-1, 0)) {
+                    this.cells_active.forEach((cell) => {
+                        // this.grid_set(cell[0], cell[1], cell_empty);
+                    });
+                    this.cells_active.forEach((cell) => {
+                        cell[0] -= 1;
+                        // this.grid_set(cell[0], cell[1], cell[2]);
+                    });
+                }
+            }
+            // Update the positions of the active cells
+            this.cells_active_display.forEach((sprite: Phaser.GameObjects.Sprite, index) => {
+                sprite.setPosition(this.col_to_x(this.cells_active[index][1]), this.row_to_y(this.cells_active[index][0]))
+            });
+        }
+        if (this.debugText != undefined) {
+            this.debugText.setText("time: " + time + "\ndelta: " + delta + "\n" + this.cells_active);
+        }
+    }
+}
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'content',
+    backgroundColor: '#253912',
+    scene: [SceneBackground, SceneGrid]
+};
+
+const game = new Phaser.Game(config);
